d1d2f3ffb16a8132a77210eade868bb27743fc49
g.jexembayeva <g.jexembayeva@petrel.ai>
2021-05-25 20:39:16 +0600

    fix

--- BasicSortingAlgorithms/HeapSortTask.cs
+++ BasicSortingAlgorithms/HeapSortTask.cs
@@ -1,5 +1,4 @@
-﻿using DataStructures;
-using System;
+﻿using System;
 
 namespace SortingAlgorithms
 {
@@ -7,9 +6,29 @@ namespace SortingAlgorithms
     {
         public override void Sort()
         {
-            var heap = new Heap<T>(Items);
-            var sorted = heap.Order();
-            Items = sorted;
+            for (int root = Items.Count / 2 - 1; root >= 0; root--)
+            {
+                MoveMaxToRoot(root, Items.Count);
+            }
+            for (int j = Items.Count - 1; j >= 0; j--)
+            {
+                Swap(0, j);
+                MoveMaxToRoot(0, j);
+            }
+        }
+
+        private void MoveMaxToRoot(int root, int size)
+        {
+            int L = 2 * root + 1;
+            int R = 2 * root + 2;
+            int X = root;
+            if (L < size && Items[X].CompareTo(Items[L]) == -1) X = L;
+            if (R < size && Items[X].CompareTo(Items[R]) == -1) X = R;
+
+            if (X == root)
+                return;
+            Swap(X, root);
+            MoveMaxToRoot(X, size);
         }
     }
 }
